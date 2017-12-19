90ead80398b9da9d753406efa83ddf36db164a2b
Meyric Rawlings <meyric.rawlings@asset.tv>
2017-12-19 11:10:19 +0000

    Initial commit.

---
+++ src/js/index.js
@@ -0,0 +1,2 @@
+// import image from '../images/logo-on-white-bg.png';
+console.log('inner script says hi!');
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/js/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/html/index.html'
+    })
+  ]
+};
